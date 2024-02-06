087f8c0265847e6658d8102ad868cb936887c22d
armagansenol <armagansnl@gmail.com>
2024-02-06 16:34:30 +0300

    add font preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,122 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="/fonts/Ageya-Oblique.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/Ageya-Oblique.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Bold.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Bold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-ExtraBold.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-ExtraBold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Medium.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Medium.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Regular.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Regular.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Light.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VanguardCF-Light.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VisbyCF-Regular.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VisbyCF-Regular.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VisbyCF-DemiBold.woff"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/VisbyCF-DemiBold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
