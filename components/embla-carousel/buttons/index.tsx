caf545d065e85dc598d1fd187c7d49a54a255f40
armagansenol <armagansnl@gmail.com>
2024-02-16 17:27:15 +0300

    misc

--- components/embla-carousel/buttons/index.tsx
+++ components/embla-carousel/buttons/index.tsx
@@ -1,10 +1,7 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from "react"
 
 type PropType = PropsWithChildren<
-  React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  >
+  React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 >
 
 export const DotButton: React.FC<PropType> = (props) => {
@@ -21,11 +18,7 @@ export const PrevButton: React.FC<PropType> = (props) => {
   const { children, ...restProps } = props
 
   return (
-    <button
-      className="embla__button embla__button--prev"
-      type="button"
-      {...restProps}
-    >
+    <button className="embla__button embla__button--prev" type="button" {...restProps}>
       {children}
     </button>
   )
@@ -35,11 +28,7 @@ export const NextButton: React.FC<PropType> = (props) => {
   const { children, ...restProps } = props
 
   return (
-    <button
-      className="embla__button embla__button--next"
-      type="button"
-      {...restProps}
-    >
+    <button className="embla__button embla__button--next" type="button" {...restProps}>
       {children}
     </button>
   )
--- next.config.js
+++ next.config.js
@@ -16,18 +16,6 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import 'styles/_functions';`,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/contact",
-        destination: "/contact",
-      },
-      {
-        source: "/iletisim",
-        destination: "/contact",
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
