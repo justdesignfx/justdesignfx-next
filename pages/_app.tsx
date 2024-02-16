4a3834a5345bd4366d058beff07c2c3d379781e4
armagansenol <armagansnl@gmail.com>
2024-02-16 16:50:37 +0300

    remove page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import "../styles/global.scss"
 
-import PageTransition, { useAsPathWithoutHash } from "@madeinhaus/nextjs-page-transition"
 import { QueryClient, QueryClientProvider } from "react-query"
-import "@madeinhaus/nextjs-page-transition/dist/index.css"
 
 import { SmoothLayout } from "@/layouts/smooth"
 
@@ -11,14 +9,10 @@ import type { AppProps } from "next/app"
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
-  const key = useAsPathWithoutHash()
-
   return (
     <QueryClientProvider client={queryClient}>
       <SmoothLayout>
-        <PageTransition>
-          <Component {...pageProps} key={key} />
-        </PageTransition>
+        <Component {...pageProps} />
       </SmoothLayout>
     </QueryClientProvider>
   )
