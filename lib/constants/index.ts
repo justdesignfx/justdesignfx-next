2a62b66c3089da13a56001cf533772017ecdc31b
armagansenol <armagansnl@gmail.com>
2024-02-16 10:33:15 +0300

    add routes

---
+++ lib/constants/index.ts
@@ -0,0 +1,43 @@
+export const routes = {
+  home: {
+    ariaLabel: "Homepage",
+    name: "home",
+    path: "",
+    ui: "HOME",
+    seo: {
+      title: "Chamaeleon | Wholistic Investments",
+      description: "We are an early-stage VC firm based in Silicon Valley, investing globally.",
+    },
+  },
+  about: {
+    ariaLabel: "About",
+    name: "about",
+    path: "about",
+    ui: "ABOUT",
+    seo: {
+      title: "Chamaeleon | Manifesto & Values",
+      description:
+        "Chamaeleon is a new early-stage venture capital firm that brings together partners that have long collaborated as investors, operators and entrepreneurs that will focus on investing in truly transformative companies.",
+    },
+  },
+  works: {
+    ariaLabel: "Our Works",
+    name: "works",
+    path: "works",
+    ui: "WORKS",
+    seo: {
+      title: "Chamaeleon | Team",
+      description: "We are an early-stage VC firm based in Silicon Valley, investing globally.",
+    },
+  },
+  contact: {
+    ariaLabel: "Contact Us",
+    name: "contact",
+    path: "contact",
+    ui: "LET'S TALK",
+    seo: {
+      title: "Chamaeleon | Approach",
+      description: "Start-ups are changing. It's time for venture capitals to change.",
+    },
+  },
+}
