c37c488312a7655c60f53cf3ee959bd6bd657fef
armagansenol <armagansnl@gmail.com>
2024-02-21 09:27:28 +0300

    update responsive styles

--- components/card-member/CardMember.tsx
+++ components/card-member/CardMember.tsx
@@ -10,10 +10,10 @@ import { IconLinkedin } from "@/components/icons/icon-linkedin"
 import { TeamMemberProps } from "@/types"
 
 const CardMember = (props: TeamMemberProps) => {
-  const { id, name, surname, role, profileImage, iconImage, linkedin } = props
+  const { name, surname, role, profileImage, iconImage, linkedin } = props
 
   return (
-    <div className={cx(s.cardMember, "flex items-center gap-7")}>
+    <div className={cx(s.cardMember, "flex flex-col tablet:flex-row items-center justify-center gap-5 tablet:gap-7")}>
       <div className={cx(s.imgC, "flex-shrink-0")}>
         <div className={s.emojiC}>
           <Img
@@ -25,15 +25,14 @@ const CardMember = (props: TeamMemberProps) => {
           />
         </div>
         <div className={s.memberPic}>
-          <Img src={profileImage} alt="Team Member" height={500} width={500} />
+          <Img className="object-cover" src={profileImage} alt="Team Member" height={500} width={500} />
         </div>
       </div>
 
-      <div className={cx(s.info, "flex flex-col items-start")}>
-        <h6 className={s.credentials}>
-          {name}
-          <br /> {surname}
-        </h6>
+      <div className={cx(s.info, "flex flex-col items-center tablet:items-start")}>
+        <p>
+          {name} <br className={cx("hidden tablet:block")} /> {surname}
+        </p>
 
         {role.split("<br>").map((par: string, i: number) => {
           return (
